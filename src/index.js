c7d9a024b7aa2a016c7351086a6516d0eb1c685b
LostHumanty <luck.smiles.at.me@gmail.com>
2024-09-25 18:46:01 +0500

    feat: created solution for this task (ср, сент. 25, 2024 06:46:01 )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  const list = [];
+  const pairBrackets = new Map(config);
+  const sameBrackets = new Set(config.filter(b => b[0] === b[1]).map(b => b[0]));
+
+  for (let symbol of str) {
+    if (pairBrackets.has(symbol)) {
+      if (sameBrackets.has(symbol)) {
+        if (list.length && list[list.length - 1] === symbol) {
+          list.pop();
+        } else {
+          list.push(symbol);
+        }
+      } else {
+        list.push(symbol);
+      }
+    } else {
+      const lastOpened = list.pop();
+      if (pairBrackets.get(lastOpened) !== symbol) {
+        return false;
+      }
+    }
+  }
+
+  return list.length === 0;
 }
